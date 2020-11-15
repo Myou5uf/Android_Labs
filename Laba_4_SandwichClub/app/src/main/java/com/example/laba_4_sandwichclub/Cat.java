971b90c9df7948f66ff62ccf3bec630669532376
Myou5uf <myxamed12l@gmail.com>
2020-11-15 16:05:11 +0700

    Looked at the GSON tutorial, created a CAt class and converted this clas to JSON and then back

---
+++ Laba_4_SandwichClub/app/src/main/java/com/example/laba_4_sandwichclub/Cat.java
@@ -0,0 +1,14 @@
+package com.example.laba_4_sandwichclub;
+
+public class Cat {
+    public String name;     // Имя
+    public int age;     // Возраст
+    public int color;       // Цвет
+
+    // Конструктор
+    public Cat(){
+
+    }
+
+
+}
--- Laba_4_SandwichClub/app/src/main/java/com/example/laba_4_sandwichclub/MainActivity.java
+++ Laba_4_SandwichClub/app/src/main/java/com/example/laba_4_sandwichclub/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.laba_4_sandwichclub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    TextView tv_from;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.id_tv);
+        tv_from = findViewById(R.id.id_tv_from);
+
+
+        Cat murzik = new Cat();
+        murzik.name = "Мурзик";
+        murzik.age = 4;
+        murzik.color = Color.RED;
+
+        // Конвертация экземпляра класс Cat в JSON
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Log.i("GSON", gson.toJson(murzik));
+        String text_gson = gson.toJson(murzik).toString();
+        tv.setText(text_gson);
+
+        // Конвертация JSON в объект
+        String jsonText = "{'name':'Мурзик','color':-16777216,'age':8}";
+        Cat barsik = gson.fromJson(jsonText, Cat.class);
+        Log.i("GSON","Имя: " + barsik.name + "\nВозраст: " + barsik.age);
+        tv_from.setText("Кот: " + barsik.name + ", " + barsik.age + " лет, " + " цвет: " + barsik.color);
+
     }
 }
