323feea47c3f2a6d9cc80b82d837a6b92f83cb56
Myou5uf <myxamed12l@gmail.com>
2020-12-18 03:20:13 +0700

    Laba_5. 4) I added the functionality of the menu by implementing the classes - ButtonClick, MotionEvent, CommonGestures

--- Laba_5_EventHandling/app/src/main/java/com/example/a/event_handling/ButtonClick.java
+++ Laba_5_EventHandling/app/src/main/java/com/example/a/event_handling/ButtonClick.java
@@ -5,15 +5,48 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Objects;
+
 public class ButtonClick extends Fragment {
+
+    private TextView tv;
 
     public ButtonClick() {
         super(R.layout.activity_butclick);
     }
 
+    private final View.OnClickListener butClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            tv.setText("Button clicked");
+        }
+    };
+
+    private final View.OnLongClickListener butClickLongListener = new View.OnLongClickListener(){
+        @Override
+        public boolean onLongClick(View v){
+            tv.setText("Long button click");
+            return true;
+        }
+    };
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle saveInstanceState){
+        tv = (TextView)requireView().findViewById(R.id.but_tv);
+        Button b = getView().findViewById(R.id.button);
+        b.setOnClickListener(butClickListener);
+        b.setOnLongClickListener(butClickLongListener);
+    }
+
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle saveInstanceState){
         return inflater.inflate(R.layout.activity_butclick, parent, false);
     }
+
+
 }
--- Laba_5_EventHandling/app/src/main/java/com/example/a/event_handling/MainActivity.java
+++ Laba_5_EventHandling/app/src/main/java/com/example/a/event_handling/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.a.event_handling;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -23,5 +25,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, ButtonClick.class, null).commit();
+        }
+    }
+
+    public  boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void menu_activity(MenuItem item){
+        FragmentManager fgmg = getSupportFragmentManager();
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        Objects.requireNonNull(getSupportActionBar()).setSubtitle(item.getTitle());
+
+        switch (item.getItemId()){
+            case R.id.menuitem_bc:
+                fgmg.beginTransaction().replace(R.id.fragment_container_view, ButtonClick.class, null)
+                        .setReorderingAllowed(true).addToBackStack("name")//can be null
+                        .commit();
+                return;
+
+            case R.id.menuitem_me:
+                fgmg.beginTransaction().replace(R.id.fragment_container_view, MotionEvent_Fragment.class, null)
+                        .setReorderingAllowed(true).addToBackStack("name").commit();
+                return;
+
+            case R.id.menuitem_cg:
+                fgmg.beginTransaction().replace(R.id.fragment_container_view, CommonGestures.class, null)
+                        .setReorderingAllowed(true).addToBackStack("name").commit();
+
+        }
     }
 }
